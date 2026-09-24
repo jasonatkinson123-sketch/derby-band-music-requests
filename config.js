05cb64249b578ec6e7cdbf335e1f3511635f194e
jasonatkinson123-sketch <jasonatkinson123@gmail.com>
2026-09-24 09:08:26 -0400

    Connect student requests to Google Form

--- config.js
+++ config.js
@@ -1,2 +1,10 @@
-// Paste the deployed Google Apps Script /exec URL here.
-window.DERBY_MUSIC_CONFIG = { API_URL: "" };
+window.DERBY_MUSIC_CONFIG = {
+  API_URL: "",
+  GOOGLE_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSd7rCHUY0MaQdLaX1Yed_JCBQKJ9gNHSyE0-a5xQqicJBCjDQ/viewform",
+  GOOGLE_FORM_FIELDS: {
+    name: "entry.92196832",
+    grade: "entry.1225702646",
+    piece: "entry.1868602513",
+    instrument: "entry.950515501"
+  }
+};
